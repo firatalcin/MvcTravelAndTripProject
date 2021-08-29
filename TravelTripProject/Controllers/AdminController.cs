27f24d7237504e33cd1f1dfe9c7da6b60aabbc82
Fırat Alçın <firatalcin04@gmail.com>
2021-08-29 23:42:01 +0300

    Update

---
+++ TravelTripProject/Controllers/AdminController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProject.Models.Classes;
+
+namespace TravelTripProject.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+
+        Context c = new Context();
+
+        [Authorize]
+     
+        public ActionResult Index()
+        {
+            var degerler = c.Blogs.ToList();
+            return View(degerler);
+        }
+
+        [HttpGet]
+        public ActionResult YeniBlog()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult YeniBlog(Blog p)
+        {
+            c.Blogs.Add(p);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult BlogSil(int id)
+        {
+            var b = c.Blogs.Find(id);
+            c.Blogs.Remove(b);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult BlogGetir(int id)
+        {
+            var bl = c.Blogs.Find(id);
+            return View("BlogGetir", bl);
+        }
+
+        public ActionResult BlogGuncelle(Blog b)
+        {
+            var bl = c.Blogs.Find(b.Id);
+            bl.Aciklama = b.Aciklama;
+            bl.Baslik = b.Baslik;
+            bl.BlogImage = b.BlogImage;
+            bl.Tarih = b.Tarih;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult YorumListesi()
+        {
+            var yorumlar = c.Yorumlars.ToList();
+            return View(yorumlar);
+        }
+
+        public ActionResult YorumSil(int id)
+        {
+            var y = c.Yorumlars.Find(id);
+            c.Yorumlars.Remove(y);
+            c.SaveChanges();
+            return RedirectToAction("YorumListesi");
+        }
+
+        public ActionResult YorumGetir(int id)
+        {
+            var yorum = c.Yorumlars.Find(id);
+            return View("YorumGetir", yorum);
+        }
+
+        public ActionResult YorumGuncelle(Yorumlar y)
+        {
+            var yorum = c.Yorumlars.Find(y.Id);
+            yorum.KullaniciAdi = y.KullaniciAdi;
+            yorum.Mail = y.Mail;
+            yorum.Yorum = y.Yorum;         
+            c.SaveChanges();
+            return RedirectToAction("YorumListesi");
+        }
+
+
+    }
+}
---
+++ TravelTripProject/Controllers/DefaultController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProject.Models.Classes;
+
+namespace TravelTripProject.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+
+        Context c = new Context();
+
+        [Authorize]
+        public ActionResult Index()
+        {
+            var degerler = c.Blogs.Take(4).ToList();
+            return View(degerler);
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public PartialViewResult Partial1()
+        {
+            var degerler = c.Blogs.OrderByDescending(x => x.Id).Take(2).ToList();
+            return PartialView(degerler);
+        }
+
+        public PartialViewResult Partial2()
+        {
+            var degerler = c.Blogs.Where(x => x.Id == 1).ToList();
+            return PartialView(degerler);
+        }
+
+        public PartialViewResult Partial3()
+        {
+            var deger = c.Blogs.Take(10).ToList();
+            return PartialView(deger);
+        }
+
+        public PartialViewResult Partial4()
+        {
+            var deger = c.Blogs.Take(3).ToList();
+            return PartialView(deger);
+        }
+
+        public PartialViewResult Partial5()
+        {
+            var deger = c.Blogs.OrderByDescending(x => x.Id).Take(3).ToList();
+            return PartialView(deger);
+        }
+    }
+}
